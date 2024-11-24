e0d054c3e28e8ba28782635e79dfc07a051a8fea
Dmitri Dmitriev <d.dzmitryeu@gmail.com>
2024-11-24 11:51:04 +0200

    sections

--- src/appendToStickerSheetPage.ts
+++ src/appendToStickerSheetPage.ts
@@ -2,6 +2,7 @@ import { addToIndex } from "./buildOneSticker";
 import { lockStickers } from "./lockStickers";
 import { placeResultTopRight } from "./utilityFunctions";
 import { buildAutoLayoutFrame } from "./utilityFunctions";
+import { SECTION_FILL } from "./constants";
 
 export function appendToStickerSheetPage(
   stickerSheetPage: PageNode,
@@ -19,14 +20,25 @@ export function appendToStickerSheetPage(
   currentSectionFrame.appendChild(stickerFrame);
 
   // placeResultTopRight(stickerFrame, stickerSheetPage);
-  lockStickers(stickerFrame);
+  // lockStickers(stickerFrame);
 }
 
 function findOrCreateAllSectionsFrame() {
-  //
+  let allSectionsFrame = figma.currentPage.findChild(
+    (frame) => frame.name === "Sections"
+  );
+  if (!allSectionsFrame) {
+    allSectionsFrame = buildAutoLayoutFrame("Sections", "VERTICAL", 24, 24, 24);
+    allSectionsFrame.fills = [];
+    figma.currentPage.appendChild(allSectionsFrame);
+    placeResultTopRight(allSectionsFrame as FrameNode, figma.currentPage);
+  }
+  return allSectionsFrame as FrameNode;
 }
 
 function findOrCreateCurrentSectionFrame(sectionName: string): FrameNode {
+  const allSectionsFrame = findOrCreateAllSectionsFrame();
+
   const foundSection = figma.currentPage.findOne(
     (node): node is FrameNode =>
       node.type === "FRAME" && node.name === sectionName
@@ -36,17 +48,50 @@ function findOrCreateCurrentSectionFrame(sectionName: string): FrameNode {
     return foundSection;
   }
 
+  const sectonTitle = buildSectionTitle(sectionName);
+  const sectionContent = buildSectionContent(sectionName);
+
+  const sectionWithTitle = buildAutoLayoutFrame(
+    sectionName + " section",
+    "VERTICAL",
+    24,
+    24,
+    24
+  );
+  sectionWithTitle.fills = [];
+  sectionWithTitle.appendChild(sectonTitle);
+  sectionWithTitle.appendChild(sectionContent);
+
+  allSectionsFrame.appendChild(sectionWithTitle);
+
+  return sectionContent;
+}
+
+function buildSectionContent(sectionName: string) {
   const newSection = buildAutoLayoutFrame(
     sectionName,
     "HORIZONTAL",
-    12,
-    12,
-    12
+    24,
+    24,
+    24
   );
   newSection.name = sectionName;
-  figma.currentPage.appendChild(newSection);
-
+  newSection.fills = SECTION_FILL;
+  newSection.cornerRadius = 56;
   return newSection;
+}
+
+function buildSectionTitle(sectionName: string) {
+  const newSectionTitle = figma.createText();
+  newSectionTitle.characters = sectionName;
+  newSectionTitle.fontName = {
+    family: "Inter",
+    style: "Bold",
+  };
+  newSectionTitle.fontSize = 64;
+  newSectionTitle.fills = SECTION_FILL;
+  newSectionTitle.textCase = "UPPER";
+  return newSectionTitle;
 }
 
 function findCurrentSectionName(
