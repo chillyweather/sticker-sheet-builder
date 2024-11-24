06ad085c58fecbeb3ef88de414c04b37f2b637a2
Dmitri Dmitriev <d.dzmitryeu@gmail.com>
2024-11-24 10:09:41 +0200

    findOrBuildSection

--- src/appendToStickerSheetPage.ts
+++ src/appendToStickerSheetPage.ts
@@ -1,6 +1,7 @@
 import { addToIndex } from "./buildOneSticker";
 import { lockStickers } from "./lockStickers";
 import { placeResultTopRight } from "./utilityFunctions";
+import { buildAutoLayoutFrame } from "./utilityFunctions";
 
 export function appendToStickerSheetPage(
   stickerSheetPage: PageNode,
@@ -8,18 +9,49 @@ export function appendToStickerSheetPage(
   element: ComponentNode | ComponentSetNode,
   raster: FrameNode
 ) {
-  stickerSheetPage.appendChild(stickerFrame);
   figma.currentPage = stickerSheetPage;
 
   addToIndex(stickerSheetPage, element.name, stickerFrame, raster);
 
-  const currentSection = findCurrentSection(element);
+  const currentSectionName = findCurrentSectionName(element);
+  const currentSectionFrame =
+    findOrCreateCurrentSectionFrame(currentSectionName);
+  currentSectionFrame.appendChild(stickerFrame);
 
-  placeResultTopRight(stickerFrame, stickerSheetPage);
+  // placeResultTopRight(stickerFrame, stickerSheetPage);
   lockStickers(stickerFrame);
 }
 
-function findCurrentSection(node: ComponentNode | ComponentSetNode): string {
+function findOrCreateAllSectionsFrame() {
+  //
+}
+
+function findOrCreateCurrentSectionFrame(sectionName: string): FrameNode {
+  const foundSection = figma.currentPage.findOne(
+    (node): node is FrameNode =>
+      node.type === "FRAME" && node.name === sectionName
+  );
+
+  if (foundSection && foundSection.type === "FRAME") {
+    return foundSection;
+  }
+
+  const newSection = buildAutoLayoutFrame(
+    sectionName,
+    "HORIZONTAL",
+    12,
+    12,
+    12
+  );
+  newSection.name = sectionName;
+  figma.currentPage.appendChild(newSection);
+
+  return newSection;
+}
+
+function findCurrentSectionName(
+  node: ComponentNode | ComponentSetNode
+): string {
   const descriptionArray = node.description.split("\n");
   const docIndex = descriptionArray.findIndex((line) => line.startsWith("🗂️"));
   const currentSection = descriptionArray[docIndex + 1];
